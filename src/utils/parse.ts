1007f461900affcaff917ec2a0a788ca0665d226
alextsukanov <alextsukanov@yandex-team.ru>
2022-11-01 05:00:59 +0600

    Filtration projects itit

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -3,7 +3,7 @@ const minPattern = new RegExp('_/+|>|>=/', 'gi');
 export const parseStringForDiapazon = (str: string): {min: number, max: number} => {
     const splitted = str.split('-');
 
-    if (splitted.length === 1 || !splitted.includes('')) {
+    if (splitted.length === 1 || splitted.includes('')) {
         if (str.includes('+')) {
             return {
                 min: Number(str.split('+').filter(elem => elem)),
@@ -15,7 +15,8 @@ export const parseStringForDiapazon = (str: string): {min: number, max: number} 
                 max: Number(str.split('-').filter(elem => elem)),
             } 
         }
-    } 
+    }
+
     return {
         min: Number(str.split('-')[0]),
         max: Number(str.split('-')[1]),
