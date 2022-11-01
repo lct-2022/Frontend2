49427334bb39b238e3463ee7bcfb60f23d5cbbd0
alextsukanov <alextsukanov@yandex-team.ru>
2022-11-01 15:46:57 +0600

    index.html

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,9 +4,8 @@ import { NOT_NAVBAR_ROUTES, ROUTES } from '../../utils/routes';
 import { LOGIN_POINT, NEW_PROJECT_POINT, MENU_POINTS } from './consts';
 import { usersAvatarSelector, isUserAuthorizedSelector } from '../../store/selectors/activeUser';
 import { useSelector } from 'react-redux';
-// import { useSelector } from 'react-redux';
-import './Navbar.css';
 import {cn} from '@bem-react/classname';
+import './Navbar.css';
 
 const cName = cn('navbar');
 
@@ -14,26 +13,48 @@ function Navbar() {
     const location = useLocation();
 
     const isAuthorized = useSelector(isUserAuthorizedSelector);
+    console.log(isAuthorized);
+    
     const usersAvatar = useSelector(usersAvatarSelector);
 
     const menuPoints = useMemo(() => {
         const menuWithLogin = isAuthorized
-            ? {...MENU_POINTS, [NEW_PROJECT_POINT]: ROUTES.PROJECT}
+            ? {...MENU_POINTS, [NEW_PROJECT_POINT]: ROUTES.PROJECT_CREATE}
             : {...MENU_POINTS, [LOGIN_POINT]: ROUTES.LOGIN}
-
+        console.log(Object.entries(menuWithLogin));
+        
         return (
             <div className={cName('menu')}>
-                {Object.entries(menuWithLogin).map(([point, url], index) => (
-                    <a
-                        key={index}
-                        className={cName('menu-point')}
-                        href={url}
-                        target="_self"
-                        rel="noopener noreferrer"
-                    >
-                        {point}
-                    </a>
-                ))}
+                {Object.entries(menuWithLogin).map(([point, url], index) => {
+                    if (point !== NEW_PROJECT_POINT) {
+                        return (
+                            <a
+                                key={index}
+                                className={cName('menu-point', {as_btn: point === NEW_PROJECT_POINT})}
+                                href={url}
+                                target="_self"
+                                rel="noopener noreferrer"
+                            >
+                                {point}
+                            </a>
+                        )
+                    } else {
+                        return (
+                            <a 
+                                key={index}
+                                className={cName('menu-point', {as_btn: point === NEW_PROJECT_POINT})}
+                                href={url}
+                                target="_self"
+                                rel="noopener noreferrer"
+                            >
+                                <button>
+                                    {point}
+                                </button>
+                            </a>
+                        )
+                    }
+                })
+            }
             </div>
         )
     }, [isAuthorized]);
