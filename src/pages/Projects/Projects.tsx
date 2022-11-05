e930fae1a2bd6bac2688377283d2dd6218b3527f
alextsukanov <alextsukanov@yandex-team.ru>
2022-11-05 15:17:52 +0600

    search

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,4 +1,9 @@
 import React, { FC, useEffect, useMemo, useState } from 'react';
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+  } from 'react-query';
 import { cn } from '@bem-react/classname'
 import { useDispatch } from 'react-redux';
 import { getAllProjects, getProjects } from '../../api/platform';
@@ -14,41 +19,45 @@ import './Projects.css';
 import { useSelector } from 'react-redux';
 import { currentUserSelector } from '../../store/selectors/users';
 
-const cName = cn('projects-page')
+const cName = cn('projects-page');
+
+const queryClient = new QueryClient();
 
 interface Props {
     fromProfile?: boolean;
 }
 
 const Projects: FC<Props> = () => {
-    const currentUser = useSelector(currentUserSelector)
-
     const [allProjects, setAllProjects] = useState<ProjectData[]>([]);
-    
+
+    const {isLoading, error, data} = useQuery('allProjects', () => getAllProjects('*'));
+
     useEffect(() => {
-        getAllProjects('*')
-            .then(data => {
-                setAllProjects(data?.items?.map(project => ({...project, hidden: false})) || []);
-            })
-    }, []);
+        setAllProjects(data?.items?.map(project => ({...project, hidden: false})) || []);
+    }, [data]);
+
+    if (isLoading) return <h1>Loading</h1>
+    if (error) return <h1>error</h1>
 
     return (
-        <div className={cName()}>
-            <div className={cName('options')}>
-                <div className={cName('list')}>
-                    <ProjectsList projects={allProjects}/>
+        <QueryClientProvider client={queryClient}>
+            <div className={cName()}>
+                <div className={cName('options')}>
+                    <div className={cName('list')}>
+                        <ProjectsList projects={allProjects}/>
+                    </div>
+    
+                    {<div className={cName('filtration')}>
+                        <Filtration 
+                            projects={allProjects}
+                            setProjects={setAllProjects}
+                        />
+                    </div>}
                 </div>
-
-                {<div className={cName('filtration')}>
-                    <Filtration 
-                        projects={allProjects}
-                        setProjects={setAllProjects}
-                    />
-                </div>}
+    
+                {/* <Pagination projects={allProjects}/> */}
             </div>
-
-            {/* <Pagination projects={allProjects}/> */}
-        </div>
+        </QueryClientProvider>
     )
 }
 export default Projects;
